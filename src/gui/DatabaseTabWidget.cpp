d002b26ecc7c4edc9a48b80be782e37af26ffd4a
Kyle Manna <kyle@kylemanna.com>
2014-09-06 17:38:09 -0700

    gui: Display database write error to user
    * Present the user with more information about the error.
    
    Signed-off-by: Kyle Manna <kyle@kylemanna.com>

--- src/gui/DatabaseTabWidget.cpp
+++ src/gui/DatabaseTabWidget.cpp
@@ -262,11 +262,13 @@ void DatabaseTabWidget::saveDatabase(Database* db)
 
     if (dbStruct.saveToFilename) {
         bool result = false;
+        QString errorString;
 
         QSaveFile saveFile(dbStruct.filePath);
         if (saveFile.open(QIODevice::WriteOnly)) {
 
             if (!m_writer.writeDatabase(&saveFile, db) || m_writer.hasError()) {
+                errorString = m_writer.errorString();
                 saveFile.cancelWriting();
             }
 
@@ -278,8 +280,11 @@ void DatabaseTabWidget::saveDatabase(Database* db)
             updateTabName(db);
         }
         else {
+            if (errorString.length() == 0) {
+                saveFile.errorString();
+            }
             MessageBox::critical(this, tr("Error"), tr("Writing the database failed.") + "\n\n"
-                                 + saveFile.errorString());
+                                 + errorString);
         }
     }
     else {
